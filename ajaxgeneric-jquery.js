021229aee742d10e3b7e0305fe3a2c81ea903d14
Brent Hathaway <brent_hathaway@yahoo.com>
2012-07-25 09:13:49 -0400

    1st commit

---
+++ ajaxgeneric-jquery.js
@@ -0,0 +1,57 @@
+function getXMLHttp()
+{
+  var xmlHttp;
+
+  try
+  {
+    //Firefox, Opera 8.0+, Safari
+    xmlHttp = new XMLHttpRequest();
+  }
+  catch(e)
+  {
+    //Internet Explorer
+    try
+    {
+      xmlHttp = new ActiveXObject("Msxml2.XMLHTTP");
+    }
+    catch(e)
+    {
+      try
+      {
+        xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+      }
+      catch(e)
+      {
+        alert("Your browser does not support AJAX!")
+        return false;
+      }
+    }
+  }
+  return xmlHttp;
+}
+function MakeRequest(url,querystring, _JQueryLocation, returntype)
+{
+  var xmlHttp = getXMLHttp();
+
+  xmlHttp.onreadystatechange = function()
+  {
+    if(xmlHttp.readyState == 4 && xmlHttp.status==200)
+    {       
+      HandleResponse(xmlHttp.responseText,_JQueryLocation, returntype);
+    } 
+  }
+  xmlHttp.open("GET", url+querystring, true); 
+  xmlHttp.send(null);
+}
+
+function HandleResponse(response,_JQueryLocation, returntype)
+{
+  if (response != 'no result found') {
+    if (returntype =='val') { 
+        $(_JQueryLocation).val(response);
+    } else if (returntype == 'html'){
+        $(_JQueryLocation).html(response);         
+    }    
+  }
+}
+
